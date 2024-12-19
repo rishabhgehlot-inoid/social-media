6c486a75493ab6e8d25728046411bbaa68507b76
Rishabh Gehlot <rishabhgahlot@inoidsolutions.com>
2024-12-19 11:06:44 +0530

    `Update Vite config to disable HTTPS and update socket.io origin`

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   server: {
-    https: true,
+    // https: true,
     port: 3000,
   },
 });
